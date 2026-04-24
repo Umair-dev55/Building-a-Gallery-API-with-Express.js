5fec69cc010de6880b266f851b84d9b6a715a541
Umair Dev <umair.personal55@gmail.com>
2026-04-24 15:26:35 +0500

    Commenting

--- scr/app.js
+++ scr/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+app.use(express.json()); //Middleware
 const notes = [];
+//Post Method
 app.post("/notes", (req, res) => {
   console.log(req.body);
   res.status(201).json({
@@ -9,15 +10,20 @@ app.post("/notes", (req, res) => {
   });
   notes.push(req.body);
 });
-
+//Get Method
 app.get("/notes", (req, res) => {
   res.status(200).json({
     message: "Data Fetch Sucessfully",
     notes: notes,
   });
 });
+//Delete Method
 app.delete("/notes/:index", (req, res) => {
-  index = req.params.index;
+  const index = req.params.index;
   delete notes[index];
+  res.status(200).json({
+    message: "Note Deleted Successfully",
+  });
 });
+//Patch Method
 module.exports = app;
