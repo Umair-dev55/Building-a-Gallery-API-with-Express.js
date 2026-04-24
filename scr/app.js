2f2c00b6f42d0c52d7bd8249a34eab9913b47460
Umair Dev <umair.personal55@gmail.com>
2026-04-24 13:57:28 +0500

    get and dell api

--- scr/app.js
+++ scr/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
-app.use(express.json()); //middleware
+app.use(express.json());
 const notes = [];
 app.post("/notes", (req, res) => {
   console.log(req.body);
+  res.status(201).json({
+    message: "Notes Created Sucessfully",
+  });
   notes.push(req.body);
-  res.status(201).send("Created successfully");
 });
+
 app.get("/notes", (req, res) => {
-  res.json(notes);
+  res.status(200).json({
+    message: "Data Fetch Sucessfully",
+    notes: notes,
+  });
+});
+app.delete("/notes/:index", (req, res) => {
+  index = req.params.index;
+  delete notes[index];
 });
 module.exports = app;
